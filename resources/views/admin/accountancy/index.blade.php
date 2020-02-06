6ece1b5677f9177f992dd8b60a54d900b78d1cb7
Vicky Romadon <vickyromadon46@gmail.com>
2020-02-06 13:42:23 +0700

    update

--- resources/views/admin/accountancy/index.blade.php
+++ resources/views/admin/accountancy/index.blade.php
@@ -26,13 +26,36 @@
                             <th>Tanggal di Buat</th>
                         </tr>
                     </thead>
+                    <tfoot>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th id="income"></th>
+                            <th id="expense"></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
                 </table>
             </div>
         </div>
     </div>
 @endsection
 
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css">
+@endsection
+
 @section('js')
+    <script src="https://cdn.datatables.net/plug-ins/1.10.19/api/sum().js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
 	<script>
         jQuery(document).ready(function($){
             var table = $('#data_table').DataTable({
@@ -45,8 +68,18 @@
                     "type": "POST",
                     "data" : {}
                 },
+                "dom": 'Bfrtip',
+                "buttons": [
+                    'csv', 'excel', 'pdf', 'print'
+                ],
                 "language": {
                     "emptyTable": "Tidak Ada Data Tersedia",
+                },
+                "drawCallback": function () {
+                    var sumIncome = $('#data_table').DataTable().column(3).data().sum();
+                    var sumExpense = $('#data_table').DataTable().column(4).data().sum();
+                    $('#income').html(sumIncome);
+                    $('#expense').html(sumExpense);
                 },
                 "columns": [
                     {
